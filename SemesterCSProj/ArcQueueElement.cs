f07ded0735f011b96ce4f8d02c3cf026fef5e4c8
Tyouto <kruselaszlo@gmail.com>
2026-06-20 17:37:46 +0200

    cleanup of unused values

--- SemesterCSProj/ArcQueueElement.cs
+++ SemesterCSProj/ArcQueueElement.cs
@@ -3,11 +3,11 @@ using ScanData;
 using System.Windows.Forms;
 namespace ArcQueue {
     class ArcQueueElement {
+        //Represents an arc node waiting to be drawn after circle is finished. Contains info needed at time of creation to determine placement
         public (int x, int y) arcHostPos { get; set; }
         public (int x, int y) parentPos { get; set; }
         public double currentAngle { get; set; }
         public double angleAllowed { get; set; }
-        //public ScanDataResult hostResults { get; set; }
         public string scanAddress { get; set; }
         public Label hostAddressDisplay { get; set; }
         public ArcQueueElement() {
