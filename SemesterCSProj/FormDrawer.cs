a6e64e9536e5fd3e3b0fbc7b11853472f489e543
Tyouto <kruselaszlo@gmail.com>
2026-06-20 16:54:13 +0200

    update FormDrawer to draw arc cleanly

--- SemesterCSProj/FormDrawer.cs
+++ SemesterCSProj/FormDrawer.cs
@@ -10,6 +10,8 @@ namespace SemesterCSProj {
         public (int h, int w) nodeDims { get; set; }
         public (int x, int y) startPos { get; set; }
         public Brush hostDefaultColor { get; set; }
+        public Brush specialHostColor { get; set; }
+        public Pen defaultLineColor { get; set; }
         //Variables to control drawing arc. Current pos,connected addresses...
         public double arcAngle { get; set; }
         public (string, ScanDataResult) currentWorkingAddr { get; set; }
